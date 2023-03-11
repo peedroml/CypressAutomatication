78d7cf13499a803c2c8d8326c3526ab45d1267c3
Pedro <pedromoralesluque0@gmail.com>
2023-03-12 00:00:23 +0100

    Eq, contains y click

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -4,12 +4,25 @@ describe('Mi primera prueba',function(){
 
     it('Mi primer caso de la prueba',function(){
 
+        //Visit
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        
+        //Get y Type
         cy.get('.search-keyword').type('ca')
 
+        //Wait
         cy.wait(2000)
 
+        //Should
         cy.get('.product:visible').should('have.length',4)
+
+        //Find
+        cy.get('.products').find('.product').should('have.length',4)
+
+        //Eq, Contains y Click
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+        cy.get('.tada').click()
+
     
     })
 
