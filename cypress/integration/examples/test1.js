5b65726f22b13f810a73cf9cbfdac9630a2dc9f4
Pedro <pedromoralesluque0@gmail.com>
2023-03-11 18:29:43 +0100

    Añadido get (type & should) y wait

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -1,8 +1,15 @@
+/// <reference types="Cypress"/>
+
 describe('Mi primera prueba',function(){
 
     it('Mi primer caso de la prueba',function(){
 
-        cy.visit('https://www.escuelaartegranada.com/')
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
+        cy.get('.search-keyword').type('ca')
+
+        cy.wait(2000)
+
+        cy.get('.product:visible').should('have.length',4)
     
     })
 
